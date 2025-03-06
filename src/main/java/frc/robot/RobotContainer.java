e4307c7332c39146b366f8d177ad32ac5a0ce5a9
voidReq <jhanstedtas@gmail.com>
2025-03-05 18:39:56 -0600

    Added front limelight

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -57,8 +57,9 @@ public class RobotContainer {
     private static final Limelight LL_BF = new Limelight(LimelightType.LL4, "limelight-bf", true, true);
     private static final Limelight LL_BR = new Limelight(LimelightType.LL4, "limelight-br", true, true);
     private static final Limelight LL_BL = new Limelight(LimelightType.LL4, "limelight-bl", true, true);
+    private static final Limelight LL_FR = new Limelight(LimelightType.LL4, "limelight-fr", true, true);
 
-    public static final LimelightContainer LLContainer = new LimelightContainer(LL_BF, LL_BR, LL_BL);
+    public static final LimelightContainer LLContainer = new LimelightContainer(LL_BF, LL_BR, LL_BL, LL_FR);
 
     private final CommandXboxController driverXbox = new CommandXboxController(
             ControllerConstants.DRIVER_CONTROLLER_PORT);
